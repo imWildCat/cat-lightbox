a4241ab9240906bd88e9afa3c361bff586418191
Daohan Chong <wildcat.name@gmail.com>
2019-12-23 21:19:53 +0800

    Multiple improvements (#2)
    * Add dev:watch
    
    * Adjust flex box
    
    * Handle duplicates
    
    * Improve
    
    * Refine isShowing
    
    * Add fade in animation
    
    * Fix close button style

--- src/cat_lightbox.ts
+++ src/cat_lightbox.ts
@@ -1,46 +1,132 @@
-import { CONTAINER_CLASS_NAME } from './constants';
+import { CONTAINER_CLASS_NAME, DEFAULT_CLOSE_SYMBOL } from './constants';
 class CatLightbox {
-  private modalContainerElement?: HTMLDivElement;
-  private modalImageElement?: HTMLImageElement;
+  private modalContainerElement!: HTMLDivElement | null;
+  private imageWrapperElement!: HTMLDivElement | null;
+  private imageElement!: HTMLImageElement | null;
+  private closeButton!: HTMLDivElement | null;
 
   private isShowing = false;
+
+  private closeSymbol = DEFAULT_CLOSE_SYMBOL;
 
   constructor(private element: HTMLImageElement) {
     element.addEventListener('click', this.onOriginalElementClick.bind(this));
   }
 
   private show(): void {
+    if (this.isShowing) {
+      return;
+    }
+
     this.isShowing = true;
 
     this.modalContainerElement = document.createElement('div');
-    this.configureContainerElement(this.modalContainerElement);
-    this.modalImageElement = document.createElement('img');
-    this.configureImageElement(this.modalImageElement);
+    this.configureContainerElement();
 
-    this.modalContainerElement.appendChild(this.modalImageElement);
+    this.imageWrapperElement = document.createElement('div');
+    this.configureWrapperElement();
+
+    this.closeButton = document.createElement('div');
+    this.configureCloseButton();
+
+    this.imageElement = document.createElement('img');
+    this.configureImageElement();
+
+    this.imageWrapperElement.appendChild(this.closeButton);
+    this.imageWrapperElement.appendChild(this.imageElement);
+    this.modalContainerElement.appendChild(this.imageWrapperElement);
 
     document.body.appendChild(this.modalContainerElement);
+  }
+
+  private dismiss(): void {
+    this.modalContainerElement?.parentElement?.removeChild(this.modalContainerElement);
+
+    // Clean up references
+    this.modalContainerElement = null;
+    this.imageWrapperElement = null;
+    this.imageElement = null;
+    this.closeButton = null;
+
+    this.isShowing = false;
   }
 
   private onOriginalElementClick(): void {
     this.show();
   }
 
-  private configureContainerElement(element: HTMLDivElement): void {
-    element.className = CONTAINER_CLASS_NAME;
-
-    element.style.position = 'absolute';
-    element.style.top = '0';
-    element.style.left = '0';
-    element.style.right = '0';
-    element.style.bottom = '0';
-    element.style.overflow = 'auto';
+  private configureWrapperElement(): void {
+    const style = this.imageWrapperElement?.style;
+    if (style) {
+      style.display = 'flex';
+      style.position = 'relative';
+    }
   }
 
-  private configureImageElement(modalImageElement: HTMLImageElement): void {
-    modalImageElement.src = this.element.src;
-    modalImageElement.style.maxHeight = '100%';
-    modalImageElement.style.maxWidth = '100%';
+  private configureContainerElement(): void {
+    if (!this.modalContainerElement) {
+      return;
+    }
+    this.modalContainerElement.className = CONTAINER_CLASS_NAME;
+
+    const style = this.modalContainerElement.style;
+    style.display = 'flex';
+    style.alignItems = 'center';
+    style.justifyContent = 'center';
+    style.position = 'absolute';
+    style.top = '0';
+    style.left = '0';
+    style.right = '0';
+    style.bottom = '0';
+    style.overflow = 'auto';
+    style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+    style.opacity = '0';
+    style.transition = 'opacity 0.25s ease';
+
+    setTimeout(() => {
+      window.requestAnimationFrame(() => {
+        style.opacity = '1';
+      });
+    });
+  }
+
+  private configureImageElement(): void {
+    if (!this.imageElement) {
+      return;
+    }
+    const style = this.imageElement.style;
+    this.imageElement.src = this.element.src;
+    style.display = 'flex';
+    style.maxHeight = '100%';
+    style.maxWidth = '100%';
+  }
+
+  private configureCloseButton(): void {
+    const closeButton = this.closeButton;
+    if (!closeButton) {
+      return;
+    }
+    closeButton.addEventListener('click', this.dismiss.bind(this));
+    closeButton.innerHTML = this.closeSymbol;
+
+    const style = closeButton.style;
+
+    style.position = 'absolute';
+    style.display = 'flex';
+    style.justifyContent = 'center';
+    style.alignItems = 'center';
+
+    style.padding = '2px';
+    style.top = '-25px';
+    style.right = '-25px';
+    style.width = '23px';
+    style.height = '23px';
+    style.fontSize = '20px';
+
+    style.color = 'white';
+    style.backgroundColor = 'black';
+    style.borderRadius = '20px';
+    style.cursor = 'pointer';
   }
 }
 
--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const CONTAINER_CLASS_NAME = 'cat-lightbox-modal-container';
+export const DEFAULT_CLOSE_SYMBOL = '&times';
